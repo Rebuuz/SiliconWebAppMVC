e75906dea6ea52e97db49c617cbd2f191bc6543d
Rebecca <rebeccalagerr@outlook.com>
2024-03-14 17:03:27 +0100

    Fixing up the sign in page and validation

--- SilcionWebAppMVC/Controllers/AuthController.cs
+++ SilcionWebAppMVC/Controllers/AuthController.cs
@@ -1,12 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SiliconMVC.ViewModels;
 
 namespace SiliconMVC.Controllers;
 
 public class AuthController : Controller
 {
+
+    /// <summary>
+    /// For my view 
+    /// </summary>
+    /// <returns></returns>
     [Route("/signup")]
+    [HttpGet]
     public IActionResult SignUp()
     {
-        return View();
+        var viewModel = new SignUpViewModel();
+
+
+        return View(viewModel);
+    }
+
+    /// <summary>
+    /// Routing for my form posts ... 
+    /// </summary>
+    /// <param name="model"></param>
+    /// <returns></returns>
+
+    [Route("/signup")]
+    [HttpPost]
+    public IActionResult SignUp(SignUpViewModel viewModel)
+    {
+        ///Return to same page
+        
+        if (!ModelState.IsValid)
+            return View(viewModel);
+
+        return RedirectToAction("SignIn", "Auth");
     }
 }
--- SilcionWebAppMVC/Models/SignUpFormModel.cs
+++ SilcionWebAppMVC/Models/SignUpFormModel.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using SiliconMVC.Helpers;
 
 namespace SiliconMVC.Models;
 
@@ -9,14 +11,14 @@ public class SignUpFormModel
     /// Firstname
     /// </summary>
     [Display(Name = "First Name", Prompt = "Enter your first name", Order = 0)]
-    [Required(ErrorMessage = "Invalid First Name")]
+    [Required(ErrorMessage = "First name is required")]
     public string FirstName { get; set; } = null!;
 
     /// <summary>
     /// Lastname
     /// </summary>
     [Display(Name = "Last Name", Prompt = "Enter your last name", Order = 1)]
-    [Required(ErrorMessage = "Invalid Last Name")]
+    [Required(ErrorMessage = "Last name is required")]
     public string LastName { get; set; } = null!;
 
     /// <summary>
@@ -24,7 +26,8 @@ public class SignUpFormModel
     /// </summary>
     [Display(Name = "Email Address", Prompt = "Enter your email address", Order = 2)]
     [EmailAddress]
-    [RegularExpression("^[^@\\s]+@[^\\s]+\\.[^@\\s]{2,}$", ErrorMessage = "Invalid Email address")]
+    [Required(ErrorMessage = "Email address is required")]
+    [RegularExpression("^[^@\\s]+@[^\\s]+\\.[^@\\s]{2,}$", ErrorMessage = "Your email address is invalid")]
     public string EmailAddress { get; set; } = null!;
 
     /// <summary>
@@ -32,7 +35,8 @@ public class SignUpFormModel
     /// </summary>
     [Display(Name = "Password", Prompt = "Enter your password", Order = 3)]
     [DataType(DataType.Password)]
-    [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{8,}$", ErrorMessage = "Invalid password format.")]
+    [Required(ErrorMessage = "Password is required")]
+    [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{8,}$", ErrorMessage = "Invalid password, must be strong password")]
     public string Password { get; set; } = null!;
 
     /// <summary>
@@ -40,14 +44,18 @@ public class SignUpFormModel
     /// </summary>
     [Display(Name = "Password", Prompt = "Confirm your password", Order = 4)]
     [DataType(DataType.Password)]
-    [Compare(nameof(Password), ErrorMessage = "Password does not match")]
+    [Required(ErrorMessage = "Password does not match")]
+    [Compare(nameof(Password), ErrorMessage = "Your passwords does not match")]
     public string ConfirmPassword { get; set; } = null!;
 
     /// <summary>
     /// Checkbox 
     /// </summary>
     [Display(Name = "I agree to the Terms & Conditions", Order = 5)]
-    [Required(ErrorMessage = "You must agree to the Terms & Conditions")]
+    //[Required(ErrorMessage = "You must agree to the Terms & Conditions")]
+    [CheckboxRequired(ErrorMessage = "You must accept the terms and conditions to proceed.")]
     public bool TermsAndConditions { get; set; } = false;
 
 }
+
+
