6571fa568d128d12c7520a71fb2f3205e206eb6f
Rebecca <rebeccalagerr@outlook.com>
2024-03-21 12:11:11 +0100

    account init

---
+++ SilcionWebAppMVC/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiliconMVC.Controllers;
+
+public class AccountController : Controller
+{
+    ///Use later
+    //private readonly AccountService _accountService;
+
+    //public AccountController(AccountService accountService)
+    //{
+    //    _accountService = accountService;
+    //}
+}
